f0549cd4080675e1be3f12b29eab19f82ae69d37
Dervis Mehmed Barutcu <43811839+DervisMehmed@users.noreply.github.com>
2021-08-16 01:22:01 +0300

    .
    ide update, gradle update.

--- app/src/main/java/com/example/museumapp/ui/ArtObjectDetailScreen/ArtObjectDetailScreen.kt
+++ app/src/main/java/com/example/museumapp/ui/ArtObjectDetailScreen/ArtObjectDetailScreen.kt
@@ -3,8 +3,10 @@ package com.example.museumapp.ui.ArtObjectDetailScreen
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.museumapp.R
@@ -12,9 +14,17 @@ import com.example.museumapp.models.detailModels.collectionModels.ArtObject
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.squareup.picasso.Picasso
 
+/*
+*   TODO: progressbar implementation
+*   TODO: detailObject implementation ve verilerin duzgunce cekilmesi
+*   TODO: detailObject'in color objeleri icin customadapter yazilmasi lazim
+*   TODO: MVVM e gore ayarlamaya calis
+* */
+
 class ArtObjectDetailScreen : AppCompatActivity() {
 
     private lateinit var artObj: ArtObject
+    private lateinit var progressBar: ProgressBar
     private lateinit var sheetBehavior: BottomSheetBehavior<*>
     private lateinit var sheetLayout: ConstraintLayout
 
@@ -25,13 +35,20 @@ class ArtObjectDetailScreen : AppCompatActivity() {
         artObj = intent.extras?.get("OBJECT") as ArtObject
         intent.extras?.remove("OBJECT")
         initUI()
+        progressBar.visibility = View.VISIBLE
+
+        loadImage()
     }
 
-    private fun initUI(){
+    private fun loadImage() {
         Picasso.get()
             .load(artObj.webImage?.url)
             .into(findViewById<ImageView>(R.id.artObjimageView))
+        progressBar.visibility = View.GONE
+    }
 
+    private fun initUI(){
+        progressBar = findViewById(R.id.progressBar)
         sheetLayout = findViewById(R.id.detailBottomSheet)
         sheetBehavior = BottomSheetBehavior.from(sheetLayout)
         findViewById<TextView>(R.id.makerDetailTextView).text = artObj.principalOrFirstMaker
